5cc4d9c0f4a9b80d053a475ea5c36b6abb3b7c54
janithpiyumal1 <160152771+janithpiyumal1@users.noreply.github.com>
2025-07-31 11:17:05 +0530

    Implement chat functionality with user selection and message formatting

--- src/components/ChatContainer.jsx
+++ src/components/ChatContainer.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React,{useRef,useEffect} from 'react'
+import assets, { messagesDummyData } from '../assets/assets'
+import { formatMessageTime } from '../lib/utils';
 
-const ChatContainer = () => {
-  return (
-    <div>
-      <h2>Chat</h2>
+const ChatContainer = ({setSelctedUser,selectedUser}) => {
+  const scrollEnd = useRef();
+  useEffect(() => {
+    if (scrollEnd.current) {
+      scrollEnd.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  },[])
+  return selectedUser ? (
+    <div className='h-full overflow-scroll relative backdrop-blur-lg'>
+      { /* Header Section */}
+      <div className="flex items-center gap-3 py-3 mx-4 border-b border-stone-600">
+        <img src={assets.profile_martin} alt="" className="w-8 h-8 rounded-full" />
+        <p className="flex-1 text-lg text-white flex items-center gap-2">
+          Martin Smith
+          <span className="w-2 h-2 rounded-full bg-green-500"></span>
+        </p>
+        <img onClick={()=>setSelctedUser(null)} src={assets.arrow_icon} alt="" className="md:hidden max-w-7" />
+        <img src={assets.help_icon} alt="" className='max-md:hidden max-w-5'/>
+      </div>
+      { /* Chat Messages Section */}
+      <div className='flex flex-col h-[calc(100%-120px)] overflow-y-scroll p-3 pb-6'>
+        {messagesDummyData.map((msg, index) => (
+          <div key={index} className={`flex items-end gap-2 justify-end ${msg.senderId !== '680f50e4f10f3cd28382ecf9' && 'flex-row-reverse'}`}>
+            {msg.image ?( 
+              <img src={msg.image} alt="" className='max-w-[230px] border border-stone-600 rounded-lg overflow-hidden mb-8' />
+            ) : (
+              <p className={`p-2 max-w-[200px] md:text-sm font-light rounded-lg mb-8 break-all bg-violet-500/30 text-white 
+                ${msg.senderId === '680f50e4f10f3cd28382ecf9' ? 'rounded-br-none' : 'rounded-bl-none'}`}>
+                {msg.text}
+              </p>
+            )}
+            <div className='text-center text-xs'>
+              <img src={msg.senderId === '680f50e4f10f3cd28382ecf9' 
+                ? assets.avatar_icon:assets.profile_martin} alt="" className='w-7 rounded-full'/>
+                <p className="text-sm text-white">{formatMessageTime(msg.createdAt)}</p>
+            </div>
+          </div>
+        ))}
+        <div ref={scrollEnd}>
+
+        </div>
+      </div>
+
+    </div>
+  ) : (
+    <div className='flex flex-col items-center justify-center gap-2 text-gray-500 bg-white/10 max-md:hidden'>
+      <img src={assets.logo_icon} alt="" className='max-w-16'/>
+      <p className='text-white text-lg font-medium'>Chat Anytime</p>
     </div>
   )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import assets from '../assets/assets'
+import assets, { userDummyData } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
 
 const Sidebar = ({selectedUser,setSelectedUser}) => {
@@ -11,13 +11,38 @@ const Sidebar = ({selectedUser,setSelectedUser}) => {
                 <img src={assets.logo} alt="logo" className="max-w-40" />
                 <div className="relative py-2 group">
                     <img src={assets.menu_icon} alt="Menu" className="max-h-5 cursor-pointer" />
-                    <div className='absolute top-full right-0 z-20 w-32 p-5 rounded-md bg-[#282242] border border-gray-600  shadow-md group-hover:block'>
+                    <div className='absolute top-full right-0 z-20 w-32 p-5 rounded-md bg-[#282242] border border-gray-600 hidden group-hover:block'>
                         <p onClick={()=>navigate('/profile')} className="cursor-pointer text-sm">Edit Profile</p>
                         <hr className="my-2 border-t border-gray-500" />
                         <p className="cursor-pointer text-sm"> Logout </p>
                     </div>
                 </div>
             </div>
+            <div className="bg-[#282242] rounded-full flex items-center gap-2 py-2 px-4 mt-5">
+                <img src={assets.search_icon} alt="Search" className="w-5" />
+                <input type="text" className="bg-transparent border-b border-none outline-none text-xs placeholder-[#c8c8c8] flex-1" placeholder='Search User' />
+            </div>
+        </div>
+        <div className="flex flex-col gap-3">
+            {userDummyData.map((user, index) => ( 
+                <div onClick={() => {setSelectedUser(user)}}
+                key={index} className={`relative flex items-center gap-3 p-2 rounded-lg cursor-pointer max-sm:text-sm ${selectedUser ?._id === user._id && 'bg-[#282242]/50'}`}> 
+                    <img src={user?.profilePic || assets.avatar_icon } alt="" 
+                    className="w-[35px] aspect-[1/1] rounded-full" />
+                    <div className="flex flex-col leading-5">
+                        <p>{user?.fullName}</p>
+                        {
+                            index < 3 ?
+                            <span className="text-green-400">Online</span> 
+                            : <span className="text-red-400">Offline</span>
+                        }
+                    </div>
+                    {
+                        index > 2 && <p className="absolute top-0 right-0 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500/50">
+                            {index}</p>
+                    }
+                </div>
+            ))}
         </div>
     </div>
   )
