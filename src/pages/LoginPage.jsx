09a1ad67ff768912e540bb9acd75b482ce874366
janithpiyumal1 <160152771+janithpiyumal1@users.noreply.github.com>
2025-08-02 11:23:33 +0530

    Add authentication context and integrate with login page; update routing and environment configuration

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,6 @@
-import React,{useState} from 'react'
+import React,{useContext, useState} from 'react'
 import assets from '../assets/assets'
+import { AuthContext } from '../context/AuthContext'
 
 const LoginPage = () => {
   const [currentState, setCurrentState] = useState("Sign Up")
@@ -9,12 +10,15 @@ const LoginPage = () => {
   const [bio, setBio] = useState("")
   const [isDataSubmited, setIsDataSubmitted] = useState(false)
 
+  const {login} = useContext(AuthContext)
+
   const onSubmitHandler = (e) => {
     e.preventDefault();
     if (currentState === "Sign Up"&& !isDataSubmited) {
       setIsDataSubmitted(true)
       return;
     }
+    login(currentState === "Sign Up" ? 'signup' : 'login', {fullName, email, password, bio});
   }
   return (
     <div className='min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl'>
