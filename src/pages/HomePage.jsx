406089a0325a7dfb8ed15d684e4705ab2b353b91
janithpiyumal1 <160152771+janithpiyumal1@users.noreply.github.com>
2025-08-03 21:17:13 +0530

    Refactor HomePage to use ChatContext for selectedUser state management and remove unused state

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,11 +2,12 @@ import React,{useContext, useState} from 'react'
 import Sidebar from '../components/Sidebar'
 import ChatContainer from '../components/ChatContainer'
 import RightSidebar from '../components/RightSidebar'
+import { ChatContext } from '../context/ChatContext'
 
 
 const HomePage = () => {
 
-  const [selectedUser, setSelctedUser] = useState(false)
+  const {selectedUser} = useContext(ChatContext)
 
   return (
     <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
@@ -15,7 +16,7 @@ const HomePage = () => {
         'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}>
         <Sidebar />
         <ChatContainer />
-        <RightSidebar selectedUser={selectedUser} setSelectedUser={setSelctedUser}/>
+        <RightSidebar selectedUser={selectedUser}/>
       </div>
     </div>
   )
